62278545416341f6ed675757065367d4c0f5315f
kevin-garnette <noyau.imperatif@gmail.com>
2020-03-17 22:42:35 +0000

    xamppi .run issueinn quarkrrepo

---
+++ arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+/* I've created a hard link of this file pointing to /var/www/html/array.php
+*/
+
+$shells = array('beta SL', 'zeta SL', 'atom', 'stretchrainshadow', 'nano light');
+
+echo $shells[1];
+
+
+
+
+
+
+?> 
