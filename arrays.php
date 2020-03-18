c7bec093d9efa46fe01dcb0461c5532746e9c3e5
kevin-garnette <noyau.imperatif@gmail.com>
2020-03-18 17:13:25 +0000

    30 min of array in the dublin train

--- arrays.php
+++ arrays.php
@@ -5,10 +5,18 @@
 
 $shells = array('beta SL', 'zeta SL', 'atom', 'stretchrainshadow', 'nano light');
 
-echo $shells[1];
+// echo $shells[1];
 
+$cars = ['Honda', 'Toyota', 'Volvo'];
+$cars[3]='Subaru';
+// echo $cars[3];
+$cars[]='Aston Martin';
 
+// echo $cars[4];
+// echo count($cars);
+ print_r($cars);
 
+var_dump($cars);
 
 
 
