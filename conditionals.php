771e3f884730c9235d4076f02e5fc3860b9fde7a
kevin-garnette <noyau.imperatif@gmail.com>
2020-03-23 14:16:41 +0000

    php coditionals statements brief overview

---
+++ conditionals.php
@@ -0,0 +1,25 @@
+<?php
+
+// PHP Front to Back [Part 7]
+
+$strechRainShadowJackets = 5;
+
+if($strechRainShadowJackets == 3)
+	{
+		echo '3 est bien le nbr de veste strechRainShadowJackets dont jai besoin';
+	}	
+elseif($strechRainShadowJackets == 6)
+	{
+		echo '6 ca fait un peu beaucoup je trouve';
+	}
+else 
+	{
+		echo 'le chiffre enregistre actuellement nest pas 3';
+	}
+
+
+
+
+
+
+?>
